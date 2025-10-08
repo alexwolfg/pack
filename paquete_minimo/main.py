0142ed3b9e6766c403785ca7d821e559d7b0dec7
alexwolfg <alejandrolobogon@gmail.com>
2025-10-08 20:42:17 +0200

    Resolve errors while building the wheel. Add logging and remove multi-app docker

--- paquete_minimo/main.py
+++ paquete_minimo/main.py
@@ -1,7 +1,17 @@
 #Coge numero por CLI como argumentos y los multiplica, mostrandolo por pantalla
 import argparse
+import logging
 def main()-> None:
 
+    logger = logging.getLogger("_multi_app_")
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+        handlers=[
+            logging.FileHandler("app.log"),
+            logging.StreamHandler()
+        ]
+    )
     parser = argparse.ArgumentParser()
 
     #Argumentos obligatorios
@@ -18,7 +28,9 @@ def main()-> None:
 
     args = parser.parse_args()
 
+    logger.info("Parametros recogidos correctamente")
     print(f'#####Resultado {args.multiplicator*args.pair*args.over_two}#######')
+    logger.info("----Resultado mostrado----")
     
 
 if __name__ == "__main__":
