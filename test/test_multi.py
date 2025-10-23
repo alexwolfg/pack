f8805cafd5dfe47b32da92ea61930e86f61f117d
alexwolfg <alejandrolobogon@gmail.com>
2025-10-23 18:08:03 +0200

    Test_multi modification

--- test/test_multi.py
+++ test/test_multi.py
@@ -1,5 +1,9 @@
 import subprocess
 import pytest
+import sys
+
+sys.path.insert(0, "../paquete_minimo")
+
 from paquete_minimo.main import main
 
 @pytest.mark.parametrize("option", "number", [( "-m", "2")])
